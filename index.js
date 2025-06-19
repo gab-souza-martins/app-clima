ad2499838fe76ddf893fbaa215eec58043184230
Grey-B <gabriellemener@gmail.com>
2025-06-19 10:53:46 -0300

    Adicionado eventListerner para o submit de userInput. Por enquanto, sua função está vazia

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const api = "1eba88f42fbf44c584b3610b3e8d23a5";
 const userInput = document.querySelector(".userInput");
 const localInput = document.getElementById("localInput");
 const container = document.querySelector(".container");
+
+userInput.addEventListener("submit", (event) => {});
