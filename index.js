daaa6d29bb301c32a6ccfa88f82fb02a3a035e9d
Grey-B <gabriellemener@gmail.com>
2025-06-19 10:52:24 -0300

    Adicionados const userInput, const localInput, e const container, com seus valores sendo os elementos correspondentes no documento

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 const api = "1eba88f42fbf44c584b3610b3e8d23a5";
+const userInput = document.querySelector(".userInput");
+const localInput = document.getElementById("localInput");
+const container = document.querySelector(".container");
